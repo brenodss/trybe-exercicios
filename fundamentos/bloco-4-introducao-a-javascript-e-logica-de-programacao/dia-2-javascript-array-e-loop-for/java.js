ef25a51ac317d0e75ebf40b66fe32aa0c20328c7
brenodss <brenondss@gmail.com>
2022-03-08 16:11:25 -0300

    Commits acumulados sobre html, css e javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/java.js
@@ -0,0 +1,26 @@
+/* Exercicio 1 */
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+/* Exercicio 2 */
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+/* Exercicio 3 */
+menu.push('Contato');
+
+console.log(menu);
+
+/* Exercicio 3 */
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+console.log(groceryList);
+
+/* Exercicio 4 */
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names) {
+    console.log(nomes)
+}
